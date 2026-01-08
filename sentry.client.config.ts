9881ae18056c46cad2f046bae27ed2ddf906d401
HuynhCo111999 <huynhco111999@gmail.com>
2026-01-08 23:08:00 +0700

    chore: config sample traces

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,7 +3,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.VERCEL_ENV || process.env.NODE_ENV,
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0.01,
   enableLogs: true,
   replaysOnErrorSampleRate: 1,
   replaysSessionSampleRate: 0.01,
--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -3,7 +3,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   integrations: [Sentry.replayIntegration()],
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0.01,
   enableLogs: true,
   replaysOnErrorSampleRate: 1,
   replaysSessionSampleRate: 0.01,
