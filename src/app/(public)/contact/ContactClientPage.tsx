f7377842df614e8d343282003f7bd65bbbcd564e
HuynhCo111999 <huynhco111999@gmail.com>
2026-03-03 23:32:36 +0700

    feat: update content

--- src/app/(public)/contact/ContactClientPage.tsx
+++ src/app/(public)/contact/ContactClientPage.tsx
@@ -128,7 +128,7 @@ export default function ContactClientPage() {
                     <div className="flex gap-4">
                       <dt className="sr-only">Địa chỉ</dt>
                       <dd>
-                        <p className="font-medium text-gray-900">123 Đường Yoga, Quận 1</p>
+                        <p className="font-medium text-gray-900">Bình Thạnh</p>
                         <p>Thành phố Hồ Chí Minh</p>
                       </dd>
                     </div>
@@ -138,7 +138,7 @@ export default function ContactClientPage() {
                         <svg className="h-5 w-5 flex-shrink-0 text-primary-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                         </svg>
-                        <span className="ml-3 font-medium hover:text-primary-600 cursor-pointer transition-colors">+84 123 456 789</span>
+                        <span className="ml-3 font-medium hover:text-primary-600 cursor-pointer transition-colors">+84 393636143</span>
                       </dd>
                     </div>
                     <div className="flex gap-4">
@@ -147,7 +147,7 @@ export default function ContactClientPage() {
                         <svg className="h-5 w-5 flex-shrink-0 text-primary-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                         </svg>
-                        <span className="ml-3 font-medium hover:text-primary-600 cursor-pointer transition-colors">info@yenyoga.vn</span>
+                        <span className="ml-3 font-medium hover:text-primary-600 cursor-pointer transition-colors">yenyoga@gmail.com</span>
                       </dd>
                     </div>
                   </dl>
